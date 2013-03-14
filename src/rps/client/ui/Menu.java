cb35052689723e8159174a538a2830b3a745847c
f-breidenstein <felixbreidenstein@gmx.de>
2013-03-15 00:27:44 +0100

    Added the Highscore Windows and the scoring function

--- src/rps/client/ui/Menu.java
+++ src/rps/client/ui/Menu.java
@@ -3,21 +3,29 @@ package rps.client.ui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 
 import rps.client.UIController;
+import rps.highscore.Highscore;
 
 public class Menu {
 
 	private final JFrame frame;
 	private final UIController controller;
+	
 	private final JFrame aboutFrame = new JFrame("About");
+	private final JFrame highFrame = new JFrame("Highscore");
+	
 	private final JMenuBar menuBar = new JMenuBar();
+	
 	private final JMenu menuGame = new JMenu("Game");
 	private final JMenuItem menuGameNew = new JMenuItem("New");
 	private final JMenuItem menuGameSurrender = new JMenuItem("Surrender");
@@ -26,9 +34,13 @@ public class Menu {
 	private final JMenu menuTheme = new JMenu("Theme");
 	private final JMenuItem themeDefault = new JMenuItem("Default");
 	private final JMenuItem themeMinimal = new JMenuItem("Minimal");
-	
+
 	private final JMenu menuInfo = new JMenu("Info");
+	private final JMenuItem infoHigh = new JMenuItem("Highscore");
 	private final JMenuItem infoAbout = new JMenuItem("About");
+	private final JLabel[] nicks = new JLabel[10];
+	private final JLabel[] score = new JLabel[10];
+	private final JLabel[] ai = new JLabel[10];
 	
 	public Menu(JFrame frame, UIController controller) {
 
@@ -47,6 +59,7 @@ public class Menu {
 		menuBar.add(menuInfo);
 		
 		menuInfo.add(infoAbout);
+		menuInfo.add(infoHigh);
 		
 		menuTheme.add(themeDefault);
 		menuTheme.add(themeMinimal);
@@ -57,6 +70,24 @@ public class Menu {
 		menuGame.add(menuGameExit);
 		frame.setJMenuBar(menuBar);
 
+		for(int i=0;i<10;i++){
+			nicks[i] = new JLabel("-");
+			ai[i] = new JLabel("-");
+			score[i] = new JLabel("-");
+			
+			nicks[i].setBounds(20, i*30+30, 150, 20);
+			ai[i].setBounds(190, i*30+30, 100, 20);
+			score[i].setBounds(350, i*30+30, 100, 20);
+					
+			highFrame.add(nicks[i]);
+			highFrame.add(ai[i]);
+			highFrame.add(score[i]);			
+		}
+		highFrame.add(new JLabel());
+		highFrame.setLocationRelativeTo(null);
+		highFrame.setBounds(500, 300, 450, 480);
+		highFrame.setVisible(false);
+		
 		menuGameSurrender.setEnabled(false);
 		menuGameNew.setEnabled(false);
 	}
@@ -66,7 +97,6 @@ public class Menu {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				controller.handleSurrender();
-
 			}
 		});
 		menuGameNew.addActionListener(new ActionListener() {
@@ -94,6 +124,26 @@ public class Menu {
 			public void actionPerformed(ActionEvent e) {
 				controller.changeTheme("minimal");
 				controller.updateTheme();
+			}
+		});
+		infoHigh.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Highscore highscore = new Highscore();
+				ResultSet rs;
+				try {
+					rs = highscore.getHighscore();
+					int i;
+					while(rs.next()){
+						i = rs.getRow()-1;
+						nicks[i].setText(rs.getString("nick"));
+						ai[i].setText(rs.getString("ai"));
+						score[i].setText(Integer.toString(rs.getInt("score")));
+					}
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+				highFrame.setVisible(true);
 			}
 		});
 		infoAbout.addActionListener(new ActionListener() {
